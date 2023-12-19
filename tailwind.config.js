db2fab3e54719d4166480fcb04d0ea28236e8260
yuuki2ggkorn <yuuki.osawa.0215@gmail.com>
2023-12-20 04:04:40 +0900

    提出用

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./googlebooks.html",
+    "./js/*.{js,ts,jsx,tsx}"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
